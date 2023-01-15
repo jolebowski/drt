e78a1f5edd34fa5838a5f16e5d46141f9cf94376
Jordan Moreira <jordan.moreira@epitech.eu>
2023-01-15 01:28:31 +0100

    add modal for image

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-const btn = document.getElementById("menu-btn");
-const nav = document.getElementById("menu");
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+const currentYear = new Date().getFullYear()
+const modal = document.getElementById('modal')
+const modalImg = document.getElementById('modal-img')
 
-btn.addEventListener("click", () => {
-  btn.classList.toggle("open");
-  nav.classList.toggle("flex");
-  nav.classList.toggle("hidden");
-});
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open')
+  nav.classList.toggle('flex')
+  nav.classList.toggle('hidden')
+})
+
+document.getElementById('year').innerHTML = `Copyright  © ${currentYear} All Rights Reserved`
+
+const showModal = (src) => {
+  modal.classList.remove('hidden')
+  modalImg.src = src
+}
+
+const closeModal = () => {
+  modal.classList.add('hidden')
+}
