6754bdd64fc3f6e4121c73580df24098ca217e3a
Jordan Moreira <jordan.moreira@epitech.eu>
2023-01-27 20:12:58 +0100

    feat(adding filter menu and new images ): adding filter menu and new images

--- js/script.js
+++ js/script.js
@@ -23,3 +23,29 @@ const showModal = (src, text) => {
 const closeModal = () => {
   modal.classList.add('hidden')
 }
+
+const filterButtons = document.querySelectorAll('.filter-button')
+const filterAllButton = document.querySelector('.filter-all-button')
+
+const showAllImages = () => {
+  const filterImages = document.querySelectorAll('.filter-img')
+  for (let j = 0; j < filterImages.length; j++) {
+    filterImages[j].style.display = 'inline-block'
+  }
+}
+for (let i = 0; i < filterButtons.length; i++) {
+  filterButtons[i].addEventListener('click', function () {
+    const selectedCategory = this.getAttribute('data-category')
+    const filterImages = document.querySelectorAll('.filter-img')
+    for (let j = 0; j < filterImages.length; j++) {
+      if (filterImages[j].getAttribute('data-category') === selectedCategory) {
+        filterImages[j].style.display = 'inline-block'
+      } else {
+        console.log(filterImages[j].style.display)
+        filterImages[j].style.display = 'none'
+      }
+    }
+  })
+}
+
+filterAllButton.addEventListener('click', showAllImages)
